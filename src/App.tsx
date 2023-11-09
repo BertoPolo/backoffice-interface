dc1e8a2ec4cd9a73332c66fe455b9fb4fecccc92
BertoPolo <bertopolo91@gmail.com>
2023-11-09 21:54:11 +0100

    redirection to /login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "./components/Login" // Make sure the path is correct to your LoginPage component
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
