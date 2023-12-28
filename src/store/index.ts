109a5b3db6f46b4182597d82a37e8878160590f2
Alberto Polo <bertopolo91@gmail.com>
2023-12-28 13:28:51 +0100

    .

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit"
-import loginSlice from "../slices/loginSlice"
-// import usersSlice from "../slices/usersSlice"
 import storage from "redux-persist/lib/storage"
 import { encryptTransform } from "redux-persist-transform-encrypt"
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist"
+import loginSlice from "../slices/loginSlice"
+// import usersSlice from "../slices/usersSlice"
 
 const reducers = combineReducers({
   loginSlice: loginSlice,
