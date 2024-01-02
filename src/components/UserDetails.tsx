8275de2575874231dfa7d4c60fb324539a1d9b17
Alberto Polo <bertopolo91@gmail.com>
2024-01-02 16:24:29 +0100

    User's details=> displaying.

--- src/components/UserDetails.tsx
+++ src/components/UserDetails.tsx
@@ -1,41 +1,72 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { TextField, IconButton, InputAdornment, Box, Container } from "@mui/material"
+
 // import usersSlice from "../slices/usersSlice"
 
 const UserDetails = () => {
   const { id } = useParams()
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({
+    _id: "",
+    name: "",
+    username: "",
+    email: "",
+    isAdmin: false,
+    createdAt: "",
+    updatedAt: "",
+    avatar: "",
+    address: "",
+  })
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      setLoading(true)
-      try {
-        const response = await fetch(`${process.env.REACT_APP_SERVER}/users/${id}`)
-        if (!response.ok) throw new Error("User not found")
-        const data = await response.json()
-        setUser(data.data)
-      } catch (error) {
-        console.error(error)
-      } finally {
-        setLoading(false)
-      }
-    }
+  const fetchUser = async () => {
+    setLoading(true)
+    const token = sessionStorage.getItem("token")
 
+    if (!token) {
+      console.error("No token found")
+      setLoading(false)
+      return
+    }
+    try {
+      const response = await fetch(`${process.env.REACT_APP_SERVER}users?_id=${id}`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      if (!response.ok) throw new Error(`Error fetching user: ${response.status}`)
+
+      const data = await response.json()
+      if (data.length === 0) {
+        throw new Error("User not found")
+      }
+      setUser(data[0])
+    } catch (error) {
+      console.error("Fetch user error:", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
     if (id) fetchUser()
   }, [id])
 
   if (loading) return <div>Loading...</div>
-  if (!user) return <div>No user found</div>
+  // if (!user) return <div>No user found</div>
 
   return (
-    <>
-      <h1>{/* {user.avatar} {user.name} {user.lastname} */}</h1>
+    <Container style={{ paddingTop: "80px" }}>
+      {id && (
+        <h1>
+          {user.avatar} {user.name}
+        </h1>
+      )}
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, consectetur. Dignissimos aut eveniet veniam quaerat fugiat dolores labore
         exercitationem quisquam adipisci aspernatur quo nemo doloribus, assumenda molestias amet animi non.
       </p>
-    </>
+    </Container>
   )
 }
 
