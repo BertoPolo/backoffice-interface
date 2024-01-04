3d17275fe368bb00246203ec29ce0bdb890686dc
Alberto Polo <bertopolo91@gmail.com>
2024-01-04 09:37:52 +0100

    lazy components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, Suspense, lazy } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { ThemeProvider, Box } from "@mui/material"
 
 import Login from "./components/Login"
-import Home from "./components/Home"
 import Navbar from "./components/Navbar"
-import Register from "./components/Register"
-import UserDetails from "./components/UserDetails"
 import ThemeSwitch from "./components/ThemeSwitch"
 import ProtectedRoute from "./components/ProtectedRoute"
 import { darkTheme, lightTheme } from "./themes/themes"
+const Home = lazy(() => import("./components/Home"))
+const Register = lazy(() => import("./components/Register"))
+const UserDetails = lazy(() => import("./components/UserDetails"))
 
 const App: React.FC = () => {
   const [themeMode, setThemeMode] = useState<"light" | "dark">(localStorage.getItem("themeMode") === "dark" ? "dark" : "light")
@@ -35,21 +35,32 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Navigate replace to="/login" />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/register"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Register />
+                </Suspense>
+              }
+            />
             <Route
               path="/users"
               element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                </Suspense>
               }
             />
             <Route
               path="/users/:id"
               element={
-                <ProtectedRoute>
-                  <UserDetails />
-                </ProtectedRoute>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ProtectedRoute>
+                    <UserDetails />
+                  </ProtectedRoute>
+                </Suspense>
               }
             />
           </Routes>
